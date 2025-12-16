71cd8bb4e720b74ce81b3353d6784f95fcff6ee8
Jermaine Byfield <mrbyfield@gmail.com>
2025-12-16 18:48:37 +0000

    update version

--- payload-woocommerce.php
+++ payload-woocommerce.php
@@ -3,7 +3,7 @@
  * Plugin Name: Payload WooCommerce
  * Plugin URI: https://github.com/payload-code/payload-woocommerce
  * Description: Accept WooCommerce payments through Payload.com.
- * Version: 1.3.0-rc2
+ * Version: 1.3.0-rc3
  * Author: Payload
  * Author URI: https://payload.com
  * Requires Plugins: woocommerce
